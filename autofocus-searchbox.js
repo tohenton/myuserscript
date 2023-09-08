06e81baab09ec7bc1ff0e203e9e79c39cc6ace0b
tohenton <tohenton@gmail.com>
2023-09-08 11:14:31 +0900

    feature: Add autofocus-searchbox.js

---
+++ autofocus-searchbox.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         Autofocus Search Box
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Automatically focus on the search box based on domain
+// @author       tohenton
+// @include      https://*
+// @include      http://*
+// @grant        none
+// @run-at      document-idle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const DOMAIN_FOCUS_IDS = {
+        'www.amazon.co.jp': 'twotabsearchtextbox',
+    };
+
+    const domain = window.location.hostname;
+    const focusId = DOMAIN_FOCUS_IDS[domain];
+
+    if (focusId) {
+        const elementToFocus = document.getElementById(focusId);
+        if (elementToFocus) {
+            elementToFocus.focus();
+        }
+    }
+})();
