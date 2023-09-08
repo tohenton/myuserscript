62788c3767d839477300b21e9b4c73948c1e18d1
tohenton <tohenton@gmail.com>
2023-09-08 10:40:32 +0900

    feature: Add dismiss-youtube-search-recommends.js

---
+++ dismiss-youtube-search-recommends.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Dismiss youtube search recommend
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Dismiss "あなたへのおすすめ" in youtube search result
+// @author       tohenton
+// @match        https://www.youtube.com/results?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const RECOMMENDATION_TITLES = ['あなたへのおすすめ'];
+
+    let hideRecommendations = () => {
+        const recommendationTitles = document.querySelectorAll('h2 span#title');
+        recommendationTitles.forEach(title => {
+            if (RECOMMENDATION_TITLES.includes(title.textContent.trim())){
+                const dismissibleDiv = title.closest('div#dismissible');
+                if (dismissibleDiv) {
+                    dismissibleDiv.style.display = 'none';
+                }
+            }
+        });
+    };
+
+    const observer = new MutationObserver(hideRecommendations);
+    observer.observe(document.body, {
+        childList: true, subtree: true
+    });
+})();
