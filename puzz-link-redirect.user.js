9d4812d98dca9bc11ba064a562a4fd5e8033305e
tohenton <tohenton@gmail.com>
2023-09-08 10:40:15 +0900

    feature: Add puzz-link-redirect.user.js

---
+++ puzz-link-redirect.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Redirect to puzz.link
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Auto redirect to puzz.link
+// @author       tohenton
+// @match        http://pzv.jp/p.html?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pzv.jp
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const newDomain = 'https://puzz.link';
+    const currentPathAndQuery = window.location.pathname + window.location.search;
+    document.location = newDomain + currentPathAndQuery;
+})();
